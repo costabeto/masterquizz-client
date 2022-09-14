1a1f83d34ad87878d568fda63cf87a70c8afa4d7
Roberto Costa <roberto.araujo339@gmail.com>
2022-09-14 10:19:28 -0300

    Socket client setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = io('http://127.0.0.1:5000');
+
+interface IUser {
+  name: string;
+  id: string;
 }
 
-export default App;
+const Home = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [name, setName] = useState('');
+  const [userList, setUserList] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('registered', (arg) => {
+      setName(arg.name);
+    });
+
+    socket.on('clients', (arg) => {
+      setUserList(arg);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('disconnect');
+      setIsConnected(false);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const handleRename = () => {
+    socket.emit('rename', { name });
+  };
+
+  return (
+    <>
+      <h1>HOME</h1>
+      {isConnected ? <h1>Connected</h1> : <h1>Disconnected</h1>}
+
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='Informe seu nome...'
+      />
+      <button onClick={() => handleRename()}>Mudar apelido</button>
+
+      <h2>Usuários online</h2>
+      {userList.map((u) => (
+        <p key={u.id}>{u.name}</p>
+      ))}
+    </>
+  );
+};
+
+export default Home;
