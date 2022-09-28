72dd53674046197740dce6a3106bd037ac38c816
Roberto Costa <roberto.araujo339@gmail.com>
2022-09-28 14:46:18 -0300

    Question page container adjustments

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,57 +1,54 @@
-import { Flex, Radio, RadioGroup, Stack, Text, Button } from "@chakra-ui/react"
-import { useMemo, useState } from "react"
-import { IRound } from "../DTOs"
-import { useSocket } from "../hooks/useSocket"
+import { Flex, Radio, RadioGroup, Stack, Text, Button } from '@chakra-ui/react';
+import { useMemo, useState } from 'react';
+import { IRound } from '../DTOs';
+import { useSocket } from '../hooks/useSocket';
 
-interface IQuestion{
-    currentRound: IRound
+interface IQuestion {
+  currentRound: IRound;
 }
 
-const Question = ({currentRound}:IQuestion) => {
-    const {user, socket} = useSocket()
-    const [result, setResult] = useState('');
-    
-    const [isSubmited, setIsSubmited]= useState(false)
+const Question = ({ currentRound }: IQuestion) => {
+  const { user, socket } = useSocket();
+  const [result, setResult] = useState('');
 
-    const handleSubmit = () => {
-        if(!user) return
-        const answer = { 
-            user: user.id,
-            answer: result
-        }
-        socket.emit("answer", answer)
-        setIsSubmited(true)
-    }
+  const [isSubmited, setIsSubmited] = useState(false);
 
-    const options = useMemo(()=>{
-        return Object.keys(currentRound.options).map(k => ({
-            answer: k,
-            text: currentRound.options[k]
-        }))
-    },[currentRound.options])
+  const handleSubmit = () => {
+    if (!user) return;
+    const answer = {
+      user: user.id,
+      answer: result,
+    };
+    socket.emit('answer', answer);
+    setIsSubmited(true);
+  };
 
-    return (
-        <Flex
-            w='100vw'
-            h='100vh'
-            direction='column'
-            align='center'    
-        >
-            <Text
-                w={'100%'}
-            >{currentRound.question}</Text>
+  const options = useMemo(() => {
+    return Object.keys(currentRound.options).map((k) => ({
+      answer: k,
+      text: currentRound.options[k],
+    }));
+  }, [currentRound.options]);
 
-            <RadioGroup onChange={setResult} value={result}>
-                <Stack direction='column'>
-                    {options.map(option => 
-                        <Radio size='lg' disabled={isSubmited} value={option.answer}>{option.text}</Radio>
-                    )}
-                </Stack>
-            </RadioGroup>
-            
-            <Button disabled={isSubmited} onClick={handleSubmit}>Enviar</Button>
-        </Flex>
-    )
-}
+  return (
+    <Flex w='100%' h='fit-content' direction='column' align='center'>
+      <Text w={'100%'}>{currentRound.question}</Text>
 
-export default Question
+      <RadioGroup onChange={setResult} value={result}>
+        <Stack direction='column'>
+          {options.map((option) => (
+            <Radio size='lg' disabled={isSubmited} value={option.answer}>
+              {option.text}
+            </Radio>
+          ))}
+        </Stack>
+      </RadioGroup>
+
+      <Button disabled={isSubmited} onClick={handleSubmit}>
+        Enviar
+      </Button>
+    </Flex>
+  );
+};
+
+export default Question;
